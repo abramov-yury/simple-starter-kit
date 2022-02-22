804f62543472fd4a186753a782ccef2d0c72afac
Yury <forwebwork2018@gmail.com>
2022-02-22 13:12:05 +0400

    add terser-webpack-plugin

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,3 +1,4 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const {config, paths} = require(`./webpack.config.js`);
 const styles = require(`./modules/css_prod.js`);
 const cleaning = require(`./modules/cleaning.js`);
@@ -13,5 +14,12 @@ config.module.rules.push(
   styles.sassRules,
   styles.cssRules,
 );
+config.optimization = {
+  minimize: true,
+  minimizer: [new TerserPlugin({
+    extractComments: false,
+    parallel: true,
+  })],
+}
 
 module.exports = config;
